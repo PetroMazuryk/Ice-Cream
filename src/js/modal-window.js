0566878cc190379b111cc06b0177fd474f7f747d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-01-06 01:06:54 +0200

    added the location modal

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -3,12 +3,23 @@
     openReadBtn: document.querySelector('[read-modal-open]'),
     closeReadBtn: document.querySelector('[read-modal-close]'),
     modalRead: document.querySelector('[read-modal]'),
+
+    openLocationBtn: document.querySelector('[location-modal-open]'),
+    closeLocationBtn: document.querySelector('[location-modal-close]'),
+    modalLocation: document.querySelector('[location-modal]'),
   };
 
   refs.openReadBtn.addEventListener('click', toggleReadModal);
   refs.closeReadBtn.addEventListener('click', toggleReadModal);
 
+  refs.openLocationBtn.addEventListener('click', toggleLocationModal);
+  refs.closeLocationBtn.addEventListener('click', toggleLocationModal);
+
   function toggleReadModal() {
     refs.modalRead.classList.toggle('is-hidden');
+  }
+
+  function toggleLocationModal() {
+    refs.modalLocation.classList.toggle('is-hidden');
   }
 })();
